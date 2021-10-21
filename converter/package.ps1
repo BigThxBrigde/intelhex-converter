7d7bb661241d9500b895abb28e430055fed48584
jermaine007 <i77@idea7.top>
2021-10-21 15:02:32 +0800

    [UPD] Update resources and add icon

--- converter/package.ps1
+++ converter/package.ps1
@@ -1,4 +1,13 @@
-rm -Force -Recurse .\build
-rm -Force .\hexconverter.spec
-rm -Force -Recurse '../gui/pyconverter'
+if (Test-Path '.\build') {
+    rm -Force -Recurse '.\build'
+}
+
+if (Test-Path '.\hexconverter.spec') {
+    rm -Force '.\hexconverter.spec'
+}
+
+if (Test-Path '../gui/pyconverter') {
+    rm -Force -Recurse '../gui/pyconverter'
+}
+
 pyinstaller .\hexconverter.py -F --distpath '../gui/pyconverter'
